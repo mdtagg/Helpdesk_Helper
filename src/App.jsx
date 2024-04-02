7afc43a632433a760a027e06211a896e5b863c0b
Mike Tagg <mdtagg91@gmail.com>
2024-04-02 12:31:34 -0700

    boiler plate form html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 
-function App() {
 
+function App() {
 
   return (
     <>
-      <div></div>
+      <h1>The Help Desk Helper</h1>
+      <form className="main-form">
+        <label>What is the problem?</label>
+        <textarea></textarea>
+        <label>What did I expect to happen?</label>
+        <textarea></textarea>
+        <label>What have I already tried?</label>
+        <textarea></textarea>
+        <label>Why I suspect its not working</label>
+        <textarea></textarea>
+        <label>Zoom Link</label>
+        <input></input>
+      </form>
+      <div className="form-buttons">
+        <button className="send-button">Send</button>
+        <button className="clear-button">Clear</button>
+      </div>
     </>
   )
 }
